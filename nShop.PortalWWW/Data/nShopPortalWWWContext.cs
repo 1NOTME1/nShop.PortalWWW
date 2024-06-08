bbd38eef31520db594796ee412469c1ee41331f9
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-08 23:03:15 +0200

    nie wiem co tu xd

---
+++ nShop.PortalWWW/Data/nShopPortalWWWContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using nShop.Data.Data.Sklep;
+
+namespace nShop.PortalWWW.Data
+{
+    public class nShopPortalWWWContext : DbContext
+    {
+        public nShopPortalWWWContext (DbContextOptions<nShopPortalWWWContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<nShop.Data.Data.Sklep.Zamowienie> Zamowienie { get; set; } = default!;
+    }
+}
